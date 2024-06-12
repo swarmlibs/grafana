99cf586cb8e9dfe7bec58427f0a0895eb81b48df
Socheat Sok <socheatsok78@gmail.com>
2024-06-12 12:28:45 +0700

    Disable all analytics and reporting and set defaults

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -7,4 +7,14 @@ set -e
 SWARMLIBS_GF_INSTALL_PLUGINS="grafana-github-datasource,grafana-gitlab-datasource,grafana-sentry-datasource"
 export GF_INSTALL_PLUGINS="${SWARMLIBS_GF_INSTALL_PLUGINS}${GF_INSTALL_PLUGINS:+,}${GF_INSTALL_PLUGINS}"
 
+# Set default values for Grafana environment variables
+export GF_LOG_LEVEL=${GF_LOG_LEVEL:-info}
+export GF_METRICS_ENABLED=${GF_METRICS_ENABLED:-true}
+
+# Disable all analytics and reporting
+export GF_NEWS_ENABLED=${GF_NEWS_ENABLED:-false}
+export GC_CHECK_FOR_UPDATES=${GC_CHECK_FOR_UPDATES:-false}
+export GF_REPORTING_ENABLED=${GF_REPORTING_ENABLED:-false}
+export GF_SUPPORT_BUNDLES_ENABLED=${GF_SUPPORT_BUNDLES_ENABLED:-false}
+
 exec /run.sh "$@"
