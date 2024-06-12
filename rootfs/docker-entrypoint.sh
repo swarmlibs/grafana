4f31db0d4e35f245985c5bf2df005bf209cd3bcf
Socheat Sok <socheatsok78@gmail.com>
2024-06-12 13:39:44 +0700

    Set default environment variables for Grafana

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -4,17 +4,27 @@
 
 set -e
 
-SWARMLIBS_GF_INSTALL_PLUGINS="grafana-github-datasource,grafana-gitlab-datasource,grafana-sentry-datasource"
-export GF_INSTALL_PLUGINS="${SWARMLIBS_GF_INSTALL_PLUGINS}${GF_INSTALL_PLUGINS:+,}${GF_INSTALL_PLUGINS}"
+# Default credentials
+export GF_SECURITY_ADMIN_USER=${GF_SECURITY_ADMIN_USER:-grafana}
+export GF_SECURITY_ADMIN_PASSWORD=${GF_SECURITY_ADMIN_PASSWORD:-grafana}
 
 # Set default values for Grafana environment variables
 export GF_LOG_LEVEL=${GF_LOG_LEVEL:-info}
 export GF_METRICS_ENABLED=${GF_METRICS_ENABLED:-true}
+export GF_USERS_ALLOW_SIGN_UP=${GF_USERS_ALLOW_SIGN_UP:-false}
 
 # Disable all analytics and reporting
-export GF_NEWS_ENABLED=${GF_NEWS_ENABLED:-false}
-export GC_CHECK_FOR_UPDATES=${GC_CHECK_FOR_UPDATES:-false}
-export GF_REPORTING_ENABLED=${GF_REPORTING_ENABLED:-false}
+export GF_ANALYTICS_CHECK_FOR_UPDATES=${GF_ANALYTICS_CHECK_FOR_UPDATES:-false}
+export GF_ANALYTICS_FEEDBACK_LINKS_ENABLED=${GF_ANALYTICS_FEEDBACK_LINKS_ENABLED:-false}
+export GF_ANALYTICS_REPORTING_ENABLED=${GF_ANALYTICS_REPORTING_ENABLED:-false}
+export GF_NEWS_NEWS_FEED_ENABLED=${GF_NEWS_NEWS_FEED_ENABLED:-false}
 export GF_SUPPORT_BUNDLES_ENABLED=${GF_SUPPORT_BUNDLES_ENABLED:-false}
+
+# Enable feature toggles
+export GF_FEATURE_TOGGLES_ENABLE=${GF_FEATURE_TOGGLES_ENABLE:-true}
+
+# Plugins
+export SWARMLIBS_GF_INSTALL_PLUGINS="grafana-github-datasource,grafana-gitlab-datasource,grafana-sentry-datasource"
+export GF_INSTALL_PLUGINS="${SWARMLIBS_GF_INSTALL_PLUGINS}${GF_INSTALL_PLUGINS:+,}${GF_INSTALL_PLUGINS}"
 
 exec /run.sh "$@"
